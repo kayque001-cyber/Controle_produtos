d5cccf038a32f903b029b81eac45b00adb40541d
kayque001-cyber <okayque363@gmail.com>
2026-09-04 16:52:33 -0300

    atualizado/banco

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Brand, category, Product
 
-# Register your models here.
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('is_active', 'name', 'description')
+    search_fields = ('name', 'description')
+    list_filter = ('is_active',)
+
+
+
+@admin.register(category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_active', 'description')
+    search_fields = ('name', 'description')
+    list_filter = ('is_active',)
+
+
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'brand', 'category', 'is_active', 'description', 'price')
+    search_fields = ('title', 'description')
+    list_filter = ('is_active', 'brand', 'category')
